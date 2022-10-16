688e5861480013f6169b4fc14c45d3c0e4520b8d
rickie <rickiemaweu@gmail.com>
2022-10-16 16:35:58 +0300

    initial commit

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -165,7 +165,7 @@ const Login = () => {
                     <input
                       id="myInput"
                       name="password"
-                      //   type={showpass ? "text" : "password"}
+                      //   type={show-pass ? "text" : "password"}
                       onChange={handleInput}
                       className="bg-gray-100 border rounded text-xs font-medium leading-none text-gray-800 py-3 w-full pl-3 mt-2"
                     />
