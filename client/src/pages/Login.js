6272ee474d1014090fd6a11abbdd65cf5037710b
rickie <rickiemaweu@gmail.com>
2022-10-16 16:36:36 +0300

    initial commit

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -127,7 +127,7 @@ const Login = () => {
                   </label>
                   <input
                     id="lastname"
-                    aria-labelledby="lastname"
+                    aria-labelledby="last-name"
                     name="last_name"
                     type="text"
                     className="bg-gray-100 border rounded text-xs font-medium leading-none placeholder-gray-800 text-gray-800 py-3 w-full pl-3 mt-2"
